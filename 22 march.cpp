1e3c1ade459355a862687ed69c4689aafe70c9ec
Vishal Sharma <vishal2003ggg@gmail.com>
2023-03-22 10:30:21 +0530

    new files

---
+++ 22 march.cpp
@@ -0,0 +1,12 @@
+// Arrays 
+#include <stdio.h>
+int main(){
+	int myarray[5];
+	myarray[0]=1;
+	myarray[1]=2;
+	myarray[2]=3;
+	myarray[3]=4;
+	myarray[4]=5;
+	printf("%d %d %d %d %d",myarray[0],myarray[1],myarray[2],myarray[3],myarray[4]);
+	return 0;
+}
